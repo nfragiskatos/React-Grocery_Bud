933fd3e6d4a7a59cd6afee3103a9a4ac17070378
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-25 06:31:55 -0700

    Initial return of List component
    App.js
    - Fixed bug in creation of id for list item (was making a function reference instead of calling function..)
    
    List.js
    - Added prop to List.js to store list of items.
    - Setup initial return of List.js to map over the list of items and return an h2 for each item (for now)

--- src/List.js
+++ src/List.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+const List = ({ items }) => {
+	return (
+		<article>
+			{items.map((item) => {
+				return <h2 key={item.id}>{item.title}</h2>;
+			})}
+		</article>
+	);
+};
 
-export default List
+export default List;
