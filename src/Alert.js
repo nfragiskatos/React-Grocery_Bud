2676e584c9291221f81e1c241f833ee85010daa0
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-25 08:28:31 -0700

    Bug fix
    - Fixed bug where timer was not resetting when new alert being shown.

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 
-const Alert = ({ type, msg, removeAlert }) => {
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			removeAlert();
-		}, 3000);
-		return () => clearTimeout(timeout);
-	}, []);
+const Alert = ({ type, msg, removeAlert, list }) => {
+	useEffect(
+		() => {
+			const timeout = setTimeout(() => {
+				removeAlert();
+			}, 3000);
+			return () => clearTimeout(timeout);
+		},
+		[ list ]
+	);
 	return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
