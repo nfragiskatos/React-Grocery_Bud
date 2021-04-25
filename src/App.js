4d1d72c792a5b9ac2d3185bc6581459fb7ff06f6
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-25 06:14:46 -0700

    Added input and submit button to form.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,19 @@ function App () {
 		<section className="section-center">
 			<form className="grocery-form" onSubmit={handleSubmit}>
 				{alert.show && <Alert />}
+				<h3>grocery bud</h3>
+				<div className="form-control">
+					<input
+						type="text"
+						className="grocery"
+						placeholder="e.g. eggs"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+					<button type="submit" className="submit-btn">
+						{isEditing ? 'edit' : 'submit'}
+					</button>
+				</div>
 			</form>
 			<div className="grocery-container">
 				<List />
