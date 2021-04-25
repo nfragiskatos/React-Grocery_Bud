b9fd82d056339b0f4119b8833deb57ba78c95195
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-25 06:10:59 -0700

    Initial Setup
    - Declared state variables.
    - Created submit handler for form
    - return a section that contains a form and container for the list.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
-function App() {
-  return <h2>grocery bud setup</h2>
+function App () {
+	const [ name, setName ] = useState('');
+	const [ list, setList ] = useState([]);
+	const [ isEditing, setIsEditing ] = useState(false);
+	const [ editId, setEditId ] = useState(null);
+	const [ alert, setAlert ] = useState({ show: false, msg: '', type: '' });
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('submitted');
+	};
+	return (
+		<section className="section-center">
+			<form className="grocery-form" onSubmit={handleSubmit}>
+				{alert.show && <Alert />}
+			</form>
+			<div className="grocery-container">
+				<List />
+				<button className="clear-btn">Clear Items</button>
+			</div>
+		</section>
+	);
 }
 
-export default App
+export default App;
