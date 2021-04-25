94fc9b7a9de4aceadf4bae434db8f1daed33ed8f
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-25 09:02:53 -0700

    Setup local storage to remember list.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
+const getLocalStorage = () => {
+	let list = localStorage.getItem('list');
+	if (list) {
+		return JSON.parse(list);
+	}
+	return [];
+};
+
 function App () {
 	const [ name, setName ] = useState('');
-	const [ list, setList ] = useState([]);
+	const [ list, setList ] = useState(getLocalStorage());
 	const [ isEditing, setIsEditing ] = useState(false);
 	const [ editId, setEditId ] = useState(null);
 	const [ alert, setAlert ] = useState({ show: false, msg: '', type: '' });
@@ -54,6 +62,13 @@ function App () {
 		setEditId(id);
 		setName(specificItem.title);
 	};
+
+	useEffect(
+		() => {
+			localStorage.setItem('list', JSON.stringify(list));
+		},
+		[ list ]
+	);
 
 	return (
 		<section className="section-center">
